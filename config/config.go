95d1ce25763bf2d73332d9cdd638ea5d59cf6d33
Tommy Chu <tommychu2256@gmail.com>
2020-10-15 18:25:27 +0200

    Added GetConfig function

---
+++ config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// Config represents the server's settings and the configuration of Postgres database.
+type Config struct {
+	DB *DB `json:"db"`
+}
+
+// GetConfig returns configuration based on the given file.
+// The base of the file's path is at the root of the project (main.go file level).
+// Configuration file must be JSON file type (.json).
+func GetConfig(file string) (*Config, error) {
+
+	// open config file
+	f, err := os.Open(file)
+	if err != nil {
+		return nil, fmt.Errorf("can not open config file: %w", err)
+	}
+	defer f.Close()
+
+	// validate file extension
+	l := len(file)
+	if file[l-5:l] != ".json" {
+		return nil, fmt.Errorf("invalid config file type: expected '.json' extension")
+	}
+
+	// decode json
+	var cfg Config
+	err = json.NewDecoder(f).Decode(&cfg)
+	if err != nil {
+		return nil, fmt.Errorf("unable to decode config file: %w", err)
+	}
+
+	return &cfg, nil
+}
