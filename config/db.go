c68184f43d126cdb50b8e79e9fb290bb7ffb0a4d
Tommy Chu <tommychu2256@gmail.com>
2020-10-15 18:24:20 +0200

    Added database configuration type

---
+++ config/db.go
@@ -0,0 +1,10 @@
+package config
+
+// DB holds credentials of the Postgres database.
+type DB struct {
+	Host     string `json:"host"`
+	Port     int    `json:"port"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	DBName   string `json:"dbname"`
+}
