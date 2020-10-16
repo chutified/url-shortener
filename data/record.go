51684443ee582c6d978ee2417ef52168754ed726
Tommy Chu <tommychu2256@gmail.com>
2020-10-16 21:49:05 +0200

    Added debug messages

--- data/record.go
+++ data/record.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"context"
+	"fmt"
 	"time"
 )
 
@@ -19,35 +20,42 @@ type Record struct {
 
 func (s *service) AddRecord(ctx context.Context, r *Record) (*Record, error) {
 	//TODO
+	fmt.Println("Added record:", *r)
 	return nil, nil
 }
 
 func (s *service) UpdateRecord(ctx context.Context, id string, r *Record) (*Record, error) {
 	//TODO
+	fmt.Printf("Updated record (id %s): %v\n", id, *r)
 	return nil, nil
 }
 
 func (s *service) DeleteRecord(ctx context.Context, id string) error {
 	//TODO
+	fmt.Printf("Deleted record id (%s)\n", id)
 	return nil
 }
 
 func (s *service) GetRecordByID(ctx context.Context, id string) (*Record, error) {
 	//TODO
+	fmt.Printf("Served record (id %s)\n", id)
 	return nil, nil
 }
 
 func (s *service) GetRecordByShort(ctx context.Context, short string) (*Record, error) {
 	//TODO
+	fmt.Printf("Served record (short %s)\n", short)
 	return nil, nil
 }
 
 func (s *service) GetRecordByFull(ctx context.Context, full string) (*Record, error) {
 	//TODO
+	fmt.Printf("Served record (full %s)\n", full)
 	return nil, nil
 }
 
 func (s *service) GetAllRecords(ctx context.Context, p int, pagin int) ([]*Record, error) {
 	//TODO
+	fmt.Printf("Served records (page %d, with pagin %d)\n", p, pagin)
 	return nil, nil
 }
