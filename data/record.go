01d9e68ecec8b54e6f430143b1b8c3a155925840
Tommy Chu <tommychu2256@gmail.com>
2020-10-16 09:32:30 +0200

    Added ID atribute for the Record model

--- data/record.go
+++ data/record.go
@@ -5,6 +5,7 @@ import "time"
 // Record is the unit of each shorten URL.  Record stores the time of its creation,
 // uppdate and deletion. All Short atributes must be unique. Full can have duplicates.
 type Record struct {
+	ID        string
 	Full      string
 	Short     string
 	Usage     int32
