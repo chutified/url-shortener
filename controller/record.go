49ae4dbb049bf91d4a5281ac4699417a6a64db62
Tommy Chu <tommychu2256@gmail.com>
2020-10-17 19:43:31 +0200

    Added gin handlers

--- controller/record.go
+++ controller/record.go
@@ -1,1 +1,31 @@
 package controller
+
+import "github.com/gin-gonic/gin"
+
+func (h *handler) AddRecord(c *gin.Context) {
+	//TODO
+}
+
+func (h *handler) UpdateRecord(c *gin.Context) {
+	//TODO
+}
+
+func (h *handler) DeleteRecord(c *gin.Context) {
+	//TODO
+}
+
+func (h *handler) GetRecordById(c *gin.Context) {
+	//TODO
+}
+
+func (h *handler) GetRecordByShort(c *gin.Context) {
+	//TODO
+}
+
+func (h *handler) GetRecordByFull(c *gin.Context) {
+	//TODO
+}
+
+func (h *handler) GetAllRecords(c *gin.Context) {
+	//TODO
+}
