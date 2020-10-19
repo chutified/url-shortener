dea9c593991fd4f87c7b1d50e30579484883bfe7
Tommy Chu <tommychu2256@gmail.com>
2020-10-19 08:59:27 +0200

    Completed main file

--- main.go
+++ main.go
@@ -3,25 +3,51 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/chutified/url-shortener/config"
-	"github.com/chutified/url-shortener/data"
+	"github.com/chutified/url-shortener/controller"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
 
+	// get configuration
 	cfg, err := config.GetConfig("config.json")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	s := data.NewService()
-	err = s.InitDB(cfg.DB)
+	// create server
+	srv := controller.NewServer()
+	err = srv.Set(cfg)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(fmt.Errorf("failed to set server: %w", err))
 	}
-	defer s.StopDB()
 
-	fmt.Println("OK")
+	// run server
+	go func() {
+		err = srv.Run()
+		if err != nil {
+			log.Println(err.Error())
+		}
+	}()
+
+	// wait for signal
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// stop server
+	if err = srv.Stop(); err != nil {
+		log.Println(err.Error())
+	}
+
+	// close connections
+	if err = srv.Close(); err != nil {
+		log.Println(err.Error())
+	}
 }
