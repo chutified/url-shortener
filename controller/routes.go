b13ea9418eccaa1133c07d0b7b971f0e5087fd37
Tommy Chu <tommychu2256@gmail.com>
2020-10-20 18:03:21 +0200

    Set routing for an API

--- controller/routes.go
+++ controller/routes.go
@@ -8,8 +8,25 @@ import (
 
 func (h *handler) getHTTPHandler() http.Handler {
 
-	// TODO
-
+	// set router
 	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	// V1
+	v1 := r.Group("/v1")
+	{
+		v1.GET("/book/:record_id", h.GetRecordById)
+		v1.GET("/book/short/:record_short", h.GetRecordByShort)
+		v1.GET("/book/full/:record_full", h.GetRecordByFull)
+
+		v1.GET("/books/len", h.GetRecordsLen)
+		v1.GET("/books", h.GetAllRecords)
+
+		v1.POST("/book", h.AddRecord)
+		v1.PUT("/book/:record_id", h.UpdateRecord)
+		v1.DELETE("/book/:record_id", h.DeleteRecord)
+	}
+
 	return r
 }
