765281045711f6d3824574b9246dbb91a7e4f7fa
Tommy Chu <tommychu2256@gmail.com>
2020-10-24 08:58:33 +0200

    Enabled CORS

--- controller/routes.go
+++ controller/routes.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"net/http"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,6 +13,7 @@ func (h *handler) getHTTPHandler() http.Handler {
 	r := gin.New()
 	r.Use(gin.Logger())
 	r.Use(gin.Recovery())
+	r.Use(cors.Default())
 
 	// V1
 	v1 := r.Group("/v1")
