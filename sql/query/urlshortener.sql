c9ea971acf8c9d87e44ca37cb4f4e9ed73ae7278
Tommy Chu <tommychu2256@gmail.com>
2020-10-31 18:42:23 +0100

    Modified database creation

--- sql/query/urlshortener.sql
+++ sql/query/urlshortener.sql
@@ -1,11 +1,12 @@
 CREATE TABLE IF NOT EXISTS shortcuts (
-  shortcut_id UUID PRIMARY KEY,
+  shortcut_id UUID NOT NULL UNIQUE,
   full_url TEXT NOT NULL,
   short_url VARCHAR(255) NOT NULL UNIQUE,
   usage INTEGER NOT NULL,
   create_at TIMESTAMP NOT NULL,
   updated_at TIMESTAMP NOT NULL,
-  deleted_at TIMESTAMP
+  deleted_at TIMESTAMP,
+  PRIMARY KEY(shortcut_id)
 );
 
 -- AddRecord
