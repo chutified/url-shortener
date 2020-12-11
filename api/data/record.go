5608938af5618cc572cab69a8514391a8e3d5f4f
Tommy Chu <tommychu2256@gmail.com>
2020-12-11 16:09:27 +0100

    Implement function which increment record's usage by one

--- api/data/record.go
+++ api/data/record.go
@@ -370,10 +370,26 @@ WHERE
 	return id, nil
 }
 
-func (s *service) incrementUsage(ctx context.Context, id string) (*Record, error) {
-	// TODO
-	fmt.Printf("Usage incremented (id %s)\n", id)
-	return nil, nil
+// incrementUsage increments usage column at record's with the given id.
+func (s *service) incrementUsage(ctx context.Context, id string) error {
+
+	// increment
+	result, err := s.DB.ExecContext(ctx, `
+UPDATE
+  shortcuts
+SET
+  usage = usage + 1
+WHERE
+  shortcut_id = $1
+  AND deleted_at IS NULL;
+	`, id)
+
+	// check changes
+	if i, _ := result.RowsAffected(); i != 1 {
+		return ErrIDNotFound
+	}
+
+	return err
 }
 
 func (s *service) TotalUsage(ctx context.Context) (int, error) {
