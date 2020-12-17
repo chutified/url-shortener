1e732e493d85acd1c60dbcd7627796c48eff8419
chutified <tommychu2256@gmail.com>
2020-12-17 21:46:06 +0100

    Move a usage functions into a new file

--- data/record.go
+++ data/record.go
@@ -412,25 +412,3 @@ WHERE
 
 	return id, nil
 }
-
-// incrementUsage increments usage column at record's with the given id.
-func (s *service) incrementUsage(ctx context.Context, id string) error {
-
-	// increment
-	result, err := s.DB.ExecContext(ctx, `
-UPDATE
-  shortcuts
-SET
-  usage = usage + 1
-WHERE
-  shortcut_id = $1
-  AND deleted_at IS NULL;
-	`, id)
-
-	// check changes
-	if i, _ := result.RowsAffected(); i != 1 {
-		return ErrIDNotFound
-	}
-
-	return err
-}
