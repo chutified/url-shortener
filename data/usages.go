8482aec4bd920357aea9199b5b3f2572e06d7b59
chutified <tommychu2256@gmail.com>
2020-12-17 21:55:49 +0100

    Implement logUsage function

--- data/usages.go
+++ data/usages.go
@@ -15,11 +15,36 @@ WHERE
   shortcut_id = $1
   AND deleted_at IS NULL;
 	`, id)
+	if err != nil {
+		return err
+	}
 
 	// check changes
 	if i, _ := result.RowsAffected(); i != 1 {
 		return ErrIDNotFound
 	}
 
-	return err
+	return nil
+}
+
+// logUsage logs the record with the given id into a usages table.
+func (s *service) logUsage(ctx context.Context, id string) error {
+
+	// store
+	result, err := s.DB.ExecContext(ctx, `
+INSERT INTO
+  usages (shortcut_id)
+VALUES
+  ($1);
+	`, id)
+	if err != nil {
+		return err
+	}
+
+	// check changes
+	if i, _ := result.RowsAffected(); i != 1 {
+		return ErrIDNotFound
+	}
+
+	return nil
 }
