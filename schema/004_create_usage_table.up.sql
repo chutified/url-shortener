bc9cb19664678f012c439604f2b3211452632545
chutified <tommychu2256@gmail.com>
2020-12-17 21:40:07 +0100

    Add usages table into database

---
+++ schema/004_create_usage_table.up.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS usages (
+  usage_id BIGSERIAL NOT NULL UNIQUE,
+  shortcut_id UUID NOT NULL,
+  logged_at TIMESTAMP NOT NULL DEFAULT NOW(),
+  PRIMARY KEY (usage_id),
+  CONSTRAINT fk_shortcut_id FOREIGN KEY (shortcut_id) REFERENCES shortcuts (shortcut_id) ON DELETE CASCADE
+);
---
+++ schema/005_create_set_usages_logged_at_trigger.down.sql
@@ -0,0 +1,4 @@
+DROP TRIGGER IF EXISTS set_logged_at_timestamp_usages
+ON usages;
+
+DROP FUNCTION IF EXISTS set_logged_at_timestamp();
