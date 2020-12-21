5f3378b524feb8ff523cd41daf33b999c34861ed
chutified <tommychu2256@gmail.com>
2020-12-21 20:41:38 +0100

    Add new admin_key generating functionality

--- data/admin.go
+++ data/admin.go
@@ -4,20 +4,39 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"math/rand"
 	"strings"
+	"time"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
+const (
+	salt = "@salt"
+
+	// generating admin_key options
+	prefixLen = 8
+	keyLen    = 40
+	digits    = "0123456789"
+	specials  = "?!@#$%^&*()[]{}<>_=-+|;:"
+	charSet   = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz" + digits + specials
+)
+
 var (
-	salt         = "@salt"
 	hashedPasswd = []byte("$2a$10$SBWWLZ4QvaTeUNk1moBW9O29Vuf4/KiXPweTcakYm4X1onaS/ZA1m")
+	username     = "urlshorteneradmin"
+
 	// ErrUnauthorized is returned if provided admin_key is invalid.
 	ErrUnauthorized = errors.New("admin key validation failure")
 )
 
 // AuthenticateAdmin validates if the given passwd is correct.
-func (s *service) AuthenticateAdmin(ctx context.Context, passwd string) error {
+func (s *service) AuthenticateAdmin(ctx context.Context, name string, passwd string) error {
+
+	// check username
+	if name != username {
+		return ErrUnauthorized
+	}
 
 	// validate
 	err := bcrypt.CompareHashAndPassword(hashedPasswd, []byte(passwd+salt))
@@ -29,6 +48,47 @@ func (s *service) AuthenticateAdmin(ctx context.Context, passwd string) error {
 
 	// success
 	return nil
+}
+
+func (s *service) GenerateAdminKey(ctx context.Context) (string, error) {
+
+	// set seed
+	rand.Seed(time.Now().UnixNano())
+
+	// init buffers
+	prefix := make([]byte, prefixLen)
+	key := make([]byte, keyLen)
+
+	// add digit
+	prefix[0] = digits[rand.Intn(len(digits))]
+	key[0] = digits[rand.Intn(len(digits))]
+
+	// add special char
+	prefix[1] = specials[rand.Intn(len(specials))]
+	key[1] = specials[rand.Intn(len(specials))]
+
+	// fill
+	for i := 2; i < prefixLen; i++ {
+		prefix[i] = charSet[rand.Intn(len(charSet))]
+	}
+	for i := 2; i < keyLen; i++ {
+		key[i] = charSet[rand.Intn(len(charSet))]
+	}
+
+	// shuffle
+	rand.Shuffle(prefixLen, func(i, j int) {
+		prefix[i], prefix[j] = prefix[j], prefix[i]
+	})
+	rand.Shuffle(keyLen, func(i, j int) {
+		key[i], key[j] = key[j], key[i]
+	})
+
+	// build
+	newKey := string(prefix) + "." + string(key)
+
+	// TODO insert into database
+
+	return newKey, nil
 }
 
 // AdminAuth validates given admin key. ErrUnauthorized is returned
