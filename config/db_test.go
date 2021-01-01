e54edff26e7527afee7830b814f3a1bfbff232a0
chutified <tommychu2256@gmail.com>
2021-01-01 11:52:58 +0100

    Add tests for dbConnStr

---
+++ config/db_test.go
@@ -0,0 +1,58 @@
+package config
+
+import (
+	"testing"
+)
+
+type fields struct {
+	DBConn string
+}
+
+var dbConnStrTests = []struct {
+	name   string
+	fields fields
+	want   string
+	want1  string
+}{
+	{
+		name: "foo db conn",
+		fields: fields{
+			DBConn: "foo",
+		},
+		want:  "postgres",
+		want1: "foo",
+	},
+	{
+		name: "bar db conn",
+		fields: fields{
+			DBConn: "bar",
+		},
+		want:  "postgres",
+		want1: "bar",
+	},
+	{
+		name: "no db conn",
+		fields: fields{
+			DBConn: "",
+		},
+		want:  "postgres",
+		want1: "",
+	},
+}
+
+func TestDB_ConnStr(t *testing.T) {
+	for _, tt := range dbConnStrTests {
+		t.Run(tt.name, func(t *testing.T) {
+			db := &DB{
+				DBConn: tt.fields.DBConn,
+			}
+			got, got1 := db.ConnStr()
+			if got != tt.want {
+				t.Errorf("ConnStr() got = %v, want %v", got, tt.want)
+			}
+			if got1 != tt.want1 {
+				t.Errorf("ConnStr() got1 = %v, want %v", got1, tt.want1)
+			}
+		})
+	}
+}
